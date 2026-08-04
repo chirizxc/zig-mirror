e4a1a00b4c177563e8185ac54083322ed60561b3
Alex Rønne Petersen <alex@alexrp.com>
2026-08-04 11:20:13 +0200

    llvm: update address spaces to LLVM 23
    Note: https://codeberg.org/ziglang/zig/issues/31190#issuecomment-20523923

--- lib/std/Target.zig
+++ lib/std/Target.zig
@@ -2337,7 +2337,6 @@ pub fn supportsAddressSpace(
         .param => is_nvptx,
         .input, .output, .uniform, .push_constant, .storage_buffer => is_spirv,
         .physical_storage_buffer => arch == .spirv64,
-        .externref, .funcref => target.cpu.has(.wasm, .reference_types),
     };
 }
 
